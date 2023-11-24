5fa87001825c65e3ab0751b5c656fa35fdcf746f
LuciaNishimiya <lucianishimiya@gmail.com>
2023-11-24 10:49:14 -0300

    add functions and error handling

--- src/services/AutoMod/index.js
+++ src/services/AutoMod/index.js
@@ -1,7 +1,19 @@
 import { DiscordUrl } from "./DiscordUrl"
+import { PhishingUrl } from "./PhishingUrl"
+import { SpamUrl } from "./SpamUrl";
 import { Honeypot } from "./honeypot"
 
 export const AutoMod = async (client, message) => {
-    DiscordUrl(client, message)
-    Honeypot(client, message)
+    await DiscordUrl(client, message).catch((error) => {
+        console.error('Error in the DiscordUrl function:', error);
+    });
+    await Honeypot(client, message).catch((error) => {
+        console.error('Error in the Honeypot function:', error);
+    });
+    await PhishingUrl(client, message).catch((error) => {
+        console.error('Error in the PhishingUrl function:', error);
+    });
+    await SpamUrl(client, message).catch((error) => {
+        console.error('Error in the SpamUrl function:', error);
+    });
 }
