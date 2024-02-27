b68fb5b7b537b5f1ac75ceb2984fe9b8c12b9fb1
LuciaNishimiya <lucianishimiya@gmail.com>
2024-02-27 03:40:06 -0300

    fix fast spam bug

--- src/services/AutoMod/SpamUrl/index.js
+++ src/services/AutoMod/SpamUrl/index.js
@@ -1,41 +1,46 @@
 import { Actions } from '../../Actions/index.js';
+import fs from 'fs';
 
-const userSpamData = new Map();
+const userSpamDataFile = 'userSpamData.json';
+
+// Check if the userSpamData file exists, if not, create it
+if (!fs.existsSync(userSpamDataFile)) {
+    fs.writeFileSync(userSpamDataFile, '{}');
+}
 
 export const Spam = async (client, message, options) => {
     const regex = /(https?:\/\/[^\s]+)/g;
     const urls = message.content.match(regex);
 
     const now = Date.now();
-
-    // Clear the map every certain interval of time
-    userSpamData.forEach((userData, authorId) => {
-        if (now - userData.timestamp > options.detection.cleanupInterval) {
-            userSpamData.delete(authorId);
-        }
-    });
+    let userSpamData = JSON.parse(fs.readFileSync(userSpamDataFile, 'utf8'));
 
     if (message.content.includes('http://') || message.content.includes('https://')) {
         const authorId = message.author.id;
         const link = urls ? urls[0] : null;
+        if (!userSpamData[authorId]) {
+            
+            userSpamData[authorId] = { link, count: 1, timestamp: now };
 
-        if (!userSpamData.has(authorId)) {
-            userSpamData.set(authorId, { link, count: 1, timestamp: now });
+            console.log(userSpamData[authorId])
+            console.log((now - userSpamData[authorId].timestamp) / 1000)
+
         } else {
-            const userData = userSpamData.get(authorId);
-
-            if (userData.link === link && now - userData.timestamp < options.detection.minSecondsBetweenLinks) {
-                userData.count++;
-
-                if (userData.count >= options.detection.maxLinks) {
-                    userSpamData.delete(authorId);
-                    const userId = message.author.id
-                    await Actions({ client, options, userId })
-
+            console.log(Math.floor((now - userSpamData[authorId].timestamp) / 1000))
+               if (Math.floor((now - userSpamData[authorId].timestamp) / 1000) <= options.detection.minSecondsBetweenLinks) {
+                userSpamData[authorId].count++;
+                console.log((now - userSpamData[authorId].timestamp) / 1000)
+                if (userSpamData[authorId].count >= options.detection.maxLinks) {
+                    delete userSpamData[authorId];
+                    const userId = message.author.id;
+                    await Actions({ client, options, userId });
                 }
             } else {
-                userSpamData.set(authorId, { link, count: 1, timestamp: now });
+                userSpamData[authorId] = { link, count: 1, timestamp: now };
             }
         }
     }
+
+    // Write the updated userSpamData back to the json file
+    fs.writeFileSync(userSpamDataFile, JSON.stringify(userSpamData));
 };
